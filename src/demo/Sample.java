68a7850acbdbe71e59f7a07d1a14c21107e6b6b1
Senthil <108150502+SenthilMurugan97@users.noreply.github.com>
2023-04-02 20:56:45 +0530

    sx

--- src/demo/Sample.java
+++ src/demo/Sample.java
@@ -1,8 +1,8 @@
 package demo;
 
 public class Sample {
-public static void main(String[] args) {
-	System.out.println("hello");
-//	""
-}
+    public static void main(String[] args) {
+        System.out.println("Modified remote code");
+        System.out.println("New line added to remote code");
+    }
 }
